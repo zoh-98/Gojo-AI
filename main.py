9850742385436c1b92c440a3852858c7e8ee068e
Gry KJ <106838880+zoh-98@users.noreply.github.com>
2024-01-12 23:20:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -27,4 +27,5 @@ def send_message():
     return jsonify({"name": name, "text": text})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 3000))
+
