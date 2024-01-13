f183be04ece383dfbe54e4b08719c22beda43df4
Gry KJ <106838880+zoh-98@users.noreply.github.com>
2024-01-13 23:12:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from flask import Flask, request, jsonify
-import os
 from characterai import PyCAI
 
 app = Flask(__name__)
 
 client = PyCAI('bb27dbf32c4d80e8043ff0248bc389212aed808f')
-char = 'dIjBbIvorS30L3fzQFha-q6khtV9S-cwrV4Nhjp-pJw'
-chat = client.chat.get_chat(char)
-participants = chat['participants']
-
-if not participants[0]['is_human']:
-    tgt = participants[0]['user']['username']
-else:
-    tgt = participants[1]['user']['username']
 
 @app.route('/api/send_message', methods=['POST'])
 def send_message():
+    char_id = request.args.get('char_id')  # Get char_id from the query parameter
+    if not char_id:
+        return jsonify({"error": "char_id is required in the query parameter"}), 400
+
+    # Use char_id to get chat information
+    chat = client.chat.get_chat(char_id)
+    participants = chat['participants']
+
+    if not participants[0]['is_human']:
+        tgt = participants[0]['user']['username']
+    else:
+        tgt = participants[1]['user']['username']
+
     data = request.json
     message = data['message']
 
